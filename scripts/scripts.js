378a5d7e8cb3ffdc66556219a859b561cce1233e
HawksP07 <austinpyzer@gmail.com>
2024-01-30 14:53:06 -0500

    Initial commit
    - Create contacts page
    - Add inputs to contacts page
    - Log inputs to console
    - Redirection added but not functional yet

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+// For the contact form
+let username = document.getElementById("nameInput");
+let phone = document.getElementById("inputPhone");
+let email = document.getElementById("inputEmail");
+let message = document.getElementById("inputMessage");
+
+function handleFormSubmit(){
+    console.log(username.value);
+    console.log(phone.value);
+    console.log(email.value);
+    console.log(message.value);
+    location.replace("index.html");
+}
